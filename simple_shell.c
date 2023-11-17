2e6cbb2cd4c24ee8f2330db9136786de0fabae8d
Sineyas92 <yssine.lchhb@gmail.com>
2023-11-17 14:26:46 -0800

    z

--- simple_shell.c
+++ simple_shell.c
@@ -1,54 +1,52 @@
 #include "main.h"
 
 /**
- * main - Simple shell.
- * @argc: arguments count.
- * @argv: arguments vector.
- *
- * Return: Nothing.
- */
-int main(int argc, char **argv)
+* main - simple_shell.
+* @ac: arguments count.
+* @argv: arguments vector.
+*
+* Return: Nothing.
+*/
+
+int main(int ac, char **argv)
 {
-    int status = 0, j;
-    char **tokens = NULL, *inputLine = NULL;
-    (void)argc; // Ignorer l'avertissement "unused parameter"
+	int status = 0, j;
+	char **cmd = NULL, *line = NULL;
+	(void) ac;
 
-    while (1)
-    {
-        inputLine = my_getline();
-        if (inputLine == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-                write(1, "\n", 1);
-            return status;
-        }
-
-        tokens = my_strtok(inputLine);
-        if (!tokens)
-            continue;
-
-        if (_strcmp(tokens[0], "exit") == 0)
-        {
-            if (tokens[1] != NULL)
-            {
-                status = _atoi(tokens[1]);
-                if (status == -1)
-                    exit(status);
-            }
-            free2d(tokens);
-            return status;
-        }
-        else if (_strcmp(tokens[0], "env") == 0)
-        {
-            for (j = 0; environ[j]; j++)
-            {
-                write(1, environ[j], _strlen(environ[j]));
-                write(1, "\n", 1);
-            }
-            free2d(tokens);
-            return status;
-        }
-        status = my_exec(tokens, argv);
-    }
+	while (1)
+	{
+		line = my_getline();
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(1, "\n", 1);
+			return (status);
+		}
+		cmd = my_strtok(line);
+		if (!cmd)
+			continue;
+		if (_strcmp(cmd[0], "exit") == 0)
+		{
+			if (cmd[1] != NULL)
+			{
+				status = _atoi(cmd[1]);
+				if (status == -1)
+					exit(status);
+			}
+			free2d(cmd);
+			return (status);
+		}
+		else if (_strcmp(cmd[0], "env") == 0)
+		{
+			for (j = 0; environ[j]; j++)
+			{
+				write(1, environ[j], _strlen(environ[j]));
+				write(1, "\n", 1);
+			}
+			free2d(cmd);
+			return (status);
+		}
+		status = my_exec(cmd, argv);
+	}
 }
-
