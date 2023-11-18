ab30fbc0e47ddc52e5242d2467efff0cbbd33c9d
Sineyas92 <yssine.lchhb@gmail.com>
2023-11-17 21:53:08 -0800

    t

--- _atoi.c
+++ _atoi.c
@@ -1,25 +1,23 @@
 #include "main.h"
-
 /**
  * _atoi - changes a string to an int
  * @s: the string to be changed
  *
  * Return: the converted int
  */
+
 int _atoi(char *c)
 {
-    int j = 1;
-    unsigned int num = 0;
-
-    do {
-        if (*c == '-')
-            j *= -1;
-        else if (*c >= '0' && *c <= '9')
-            num = num * 10 + (*c - '0');
-        else if (num > 0)
-            break;
-    } while (*c++);
-
-    return (num * j);
+int j = 1;
+unsigned int num = 0;
+do {
+if (*c == '-')
+j *= -1;
+else if (*c >= '0' && *c <= '9')
+num = num * 10 + (*c - '0');
+else if (num > 0)
+ break;
+} while (*c++);
+return (num * j);
 }
 
