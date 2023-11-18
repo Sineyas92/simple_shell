9485e8ff377dd20cc04c0d1337dbd492afdf268d
Sineyas92 <yssine.lchhb@gmail.com>
2023-11-17 23:18:56 -0800

    a

--- my_exec.c
+++ my_exec.c
@@ -2,28 +2,35 @@
 
 /**
  * my_exec - executes a program.
- * @command: vector (or array of comand strings).
+ * @command: vector (or array of command strings).
  * @arguments: argument vector.
- * Return: nothing.
+ * Return: exit status of the child process.
  */
 int my_exec(char **command, char **arguments)
 {
 pid_t child_pid;
 int child_status;
 child_pid = fork();
+
+if (child_pid == -1)
+{
+perror("Fork failed");
+return EXIT_FAILURE;
+}
 if (child_pid == 0)
 {
 if (execve(command[0], command, environ) == -1)
 {
 perror(arguments[0]);
 free2d(command);
-exit(0);
-};
+exit(EXIT_FAILURE);
 }
-else 
+}
+else
 {
 waitpid(child_pid, &child_status, 0);
-free2d(command);
+free2d(command); 
 }
-return( WEXITSTATUS(child_status));
+    return (WEXITSTATUS(child_status));
 }
+
