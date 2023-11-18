b11c62209bff8ff7a1d2310a88ebcf35b3513ad5
Sineyas92 <yssine.lchhb@gmail.com>
2023-11-18 00:00:58 -0800

    a

--- my_strtok.c
+++ my_strtok.c
@@ -2,50 +2,51 @@
 
 /**
  * my_strtok - extract tokens from a buffer.
- * @inputBuffer: input buffer.
+ * @line: input buffer.
  *
  * Return: tokens.
  */
 
-char **my_strtok(char *inputBuffer)
+char **my_strtok(char *line)
 {
-char *currentToken, **tokens, *tempBuffer;
-int count, i;
-tokens = NULL;
-currentToken = NULL;
-count = 0;
-i = 0;
-tempBuffer = NULL;
-if (!inputBuffer)
-return (NULL);
-tempBuffer = _strdup(inputBuffer);
-currentToken = strtok(tempBuffer, " \t\n");	
-if (currentToken == NULL)
-{
-free(inputBuffer), inputBuffer = NULL;
-free(tempBuffer), tempBuffer = NULL;
-return (NULL);
-}
-while (currentToken)
-{
-count++;
-currentToken = strtok(NULL, " \t\n");
-}
-free(tempBuffer), tempBuffer = NULL;
-tokens = malloc(sizeof(char *) * (count + 1));
-if (!tokens)
-{
-free(inputBuffer), inputBuffer = NULL;
-return (NULL);
-}
-currentToken = strtok(inputBuffer, " \t\n");
-while (currentToken)
-{
-tokens[i] = _strdup(currentToken);
-currentToken = strtok(NULL, " \t\n");
-i++;
-}
-free(inputBuffer), inputBuffer = NULL;
-tokens[i] = NULL;
-return (tokens);
+	char *token, **cmd, *temp;
+	int count, i;
+
+	cmd = NULL;
+	token = NULL;
+	count = 0;
+	i = 0;
+	temp = NULL;
+	if (!line)
+		return (NULL);
+	temp = _strdup(line);
+	token = strtok(temp, " \t\n");
+	if (token == NULL)
+	{
+		free(line), line = NULL;
+		free(temp), temp = NULL;
+		return (NULL);
+	}
+	while (token)
+	{
+		count++;
+		token = strtok(NULL, " \t\n");
+	}
+	free(temp), temp = NULL;
+	cmd = malloc(sizeof(char *) * (count + 1));
+	if (!cmd)
+	{
+		free(line), line = NULL;
+		return (NULL);
+	}
+	token = strtok(line, " \t\n");
+	while (token)
+	{
+		cmd[i] = _strdup(token);
+		token = strtok(NULL, " \t\n");
+		i++;
+	}
+	free(line), line = NULL;
+	cmd[i] = NULL;
+	return (cmd);
 }
